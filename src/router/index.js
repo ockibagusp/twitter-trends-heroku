2316d673d397d461e7e04ea7794914274219fbed
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-08-22 22:21:13 +0700

    index.js-> history: '/twitter-trends/'

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import GetDayTrends from '../pages/GetDayTrends.vue'
 import NotFound from '../pages/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/twitter-trends/'),
   routes: [
     {
       path: '/',
