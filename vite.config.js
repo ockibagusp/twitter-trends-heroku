ada400b000b36b9631c0bc838e39f24b3f7c2e09
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-08-22 07:31:21 +0700

    fix vite.config.js: base

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/twitter-trends/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      // string shorthand
+      '/foo': 'https://api.github.com'
     }
   }
 })
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    publicPath: "twitter-trends",
-    devServer: {
-      proxy: "https://api.github.com"
-    }
-  }
