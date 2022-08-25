33e6b1ecc51d4eebc430e83aedfde58475418c43
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-08-25 22:28:26 +0700

    add url hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CopydanPaste from '../pages/CopydanPaste.vue'
 import GetDayTrends from '../pages/GetDayTrends.vue'
 import NotFound from '../pages/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
