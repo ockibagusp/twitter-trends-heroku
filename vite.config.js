133da9de26b486a129d943403a2b3e8f1e6b7b4e
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-09-20 19:14:24 +0700

    [20/9/22] back URL base production

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/twitter-trends/',
+  base: '/twitter-trends/',
   plugins: [vue()],
   resolve: {
     alias: {
