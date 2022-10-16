dd58808c9a498f0cf09cb81507a7540abadcfaf8
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-10-16 17:36:08 +0700

    uninstall vue

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const serveStatic = require('serve-static')
 const axios = require('axios').default
 const path = require('path')
 
@@ -8,17 +7,15 @@ const app = express()
 
 app.use(cors())
 
-// here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
 // this * route is to serve project on different page routes except root `/`
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+    res.json({
+      message: 'Hi!'
+    })
 })
 
-const api = 'http://twittertrendingtopics.herokuapp.com/api/trends/Indonesia'
 app.get('/api/trends', (req, res) => {
-    axios.get(api)
+    axios.get('http://twittertrendingtopics.herokuapp.com/api/trends/Indonesia')
     .then(function (response) {
         // handle success
         res.json(response.data)
